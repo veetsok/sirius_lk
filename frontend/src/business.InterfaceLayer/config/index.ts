4761fd5b7848c8d064c477e2b91c06ac42673cbf
veetsok <ivan.kosteev19999@yandex.ru>
2024-06-12 01:58:34 +0300

    update calendar

--- frontend/src/business.InterfaceLayer/config/index.ts
+++ frontend/src/business.InterfaceLayer/config/index.ts
@@ -1,1 +1,2 @@
-export const baseUrl = "https://sirius-lk.onrender.com/lessons";
+// export const baseUrl = "https://sirius-lk.onrender.com/lessons";
+export const baseUrl = "http://localhost:5000/lessons";
--- frontend/src/user.InterfaceLayer/Libraries/Layouts/Schedule.layout/index.tsx
+++ frontend/src/user.InterfaceLayer/Libraries/Layouts/Schedule.layout/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useMemo } from "react";
 import { useSwipeable } from "react-swipeable";
 import {
-  startOfWeek,
-  addWeeks,
   format,
-  eachDayOfInterval,
-  endOfWeek,
+  parse,
   addDays,
+  startOfMonth,
+  eachWeekOfInterval,
+  endOfMonth,
+  addMonths,
 } from "date-fns";
 import { ru } from "date-fns/locale";
 import TextAtom from "../../UI_KIT/Atoms/Text.Atom";
@@ -25,32 +26,27 @@ const ScheduleLayout: React.FC<ScheduleLayoutProps> = (props) => {
   const { lessons } = props;
 
   const daysOfWeek = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вc"];
-  const [currentWeek, setCurrentWeek] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(new Date());
 
-  const startOfCurrentWeek = startOfWeek(currentWeek, { weekStartsOn: 1 }); // Monday as the start of the week
+  const startOfCurrentMonth = startOfMonth(currentMonth); // Начало текущего месяца
 
   const weeks = useMemo(
     () =>
-      Array.from({ length: 5 }, (_, i) =>
-        eachDayOfInterval({
-          start: addDays(startOfCurrentWeek, i * 7),
-          end: endOfWeek(addDays(startOfCurrentWeek, i * 7), {
-            weekStartsOn: 1,
-          }),
-        })
-      ),
-    [startOfCurrentWeek]
+      eachWeekOfInterval({
+        start: startOfCurrentMonth,
+        end: endOfMonth(startOfCurrentMonth),
+      }),
+    [startOfCurrentMonth]
   );
 
   const handlers = useSwipeable({
-    onSwipedLeft: () => setCurrentWeek(addWeeks(currentWeek, 1)),
-    onSwipedRight: () => setCurrentWeek(addWeeks(currentWeek, -1)),
+    onSwipedLeft: () => setCurrentMonth(addMonths(currentMonth, 1)),
+    onSwipedRight: () => setCurrentMonth(addMonths(currentMonth, -1)),
   });
-
   const getLessonsForDay = (day: Date) => {
     return lessons?.filter(
       (lesson) =>
-        format(new Date(lesson.date), "yyyy-MM-dd") ===
+        format(parse(lesson.date, "dd.MM.yyyy", new Date()), "yyyy-MM-dd") ===
         format(day, "yyyy-MM-dd")
     );
   };
@@ -60,23 +56,23 @@ const ScheduleLayout: React.FC<ScheduleLayoutProps> = (props) => {
       <div className="w-fit flex gap-8 items-center">
         <div className="flex items-center gap-5">
           <ImageAtom
-            onClick={() => setCurrentWeek(addWeeks(currentWeek, -1))}
+            onClick={() => setCurrentMonth(addMonths(currentMonth, -1))}
             type={ImageEnum.enum_defaultSvg}
             className="[&>svg]:stroke-text_primary w-4 h-3 rotate-180 cursor-pointer"
             icon={<ArrowIcon />}
           />
           <TextAtom type={TextAtomEnum.enum_h4} className="font-bold">
-            {format(startOfCurrentWeek, "MMMM yyyy", { locale: ru })}
+            {format(startOfCurrentMonth, "MMMM yyyy", { locale: ru })}
           </TextAtom>
           <ImageAtom
-            onClick={() => setCurrentWeek(addWeeks(currentWeek, 1))}
+            onClick={() => setCurrentMonth(addMonths(currentMonth, 1))}
             type={ImageEnum.enum_defaultSvg}
             className="[&>svg]:stroke-text_primary w-4 h-3 cursor-pointer"
             icon={<ArrowIcon />}
           />
         </div>
         <div>
-          <button onClick={() => setCurrentWeek(new Date())}>Сегодня</button>
+          <button onClick={() => setCurrentMonth(new Date())}>Сегодня</button>
           {/* <QuestionsIcon isActive={false} className={icon} /> */}
         </div>
       </div>
@@ -95,39 +91,42 @@ const ScheduleLayout: React.FC<ScheduleLayoutProps> = (props) => {
             </th>
           ))}
           <tbody className="h-full">
-            {weeks.map((week, weekIndex) => (
-              <tr key={weekIndex}>
-                {week.map((day: Date) => (
-                  <td
-                    key={day.toISOString()}
-                    className="w-[14%] h-[88px] pt-1 px-[2px] pb-[2px] relative"
-                  >
-                    <TextAtom
-                      type={TextAtomEnum.enum_h6}
-                      className="text-[#C8C5CD] absolute top-[3px] right-[3px] text-right"
+            {weeks.map((day, rowIndex) => (
+              <tr key={rowIndex}>
+                {daysOfWeek.map((_, dayIndex) => {
+                  const currentDate = addDays(day, dayIndex);
+                  return (
+                    <td
+                      key={currentDate.toISOString()}
+                      className="w-[14%] h-[88px] pt-1 px-[2px] pb-[2px] relative"
                     >
-                      {format(day, "d")}
-                    </TextAtom>
-                    {getLessonsForDay(day)?.map((lesson, index) => (
-                      <div
-                        key={index}
-                        className="flex flex-col relative bg-[#BBE7B9] h-[34px] p-1 text-left rounded-[2px] gap-1 overflow-hidden border-[0.5px] border-solid border-[#bbe7b9]"
+                      <TextAtom
+                        type={TextAtomEnum.enum_h6}
+                        className="text-[#C8C5CD] absolute top-[3px] right-[3px] text-right"
                       >
-                        <TextAtom type={TextAtomEnum.enum_h6}>
-                          {lesson.date}
-                        </TextAtom>
-                        <TextAtom type={TextAtomEnum.enum_h6}>
-                          {lesson.title}
-                        </TextAtom>
-                        <ImageAtom
-                          type={ImageEnum.enum_defaultSvg}
-                          icon={<PaymentIcon />}
-                          className="[&>svg]:stroke-[#E12828] w-[14px] h-[14px] absolute top-[3px] right-[3px]"
-                        />
-                      </div>
-                    ))}
-                  </td>
-                ))}
+                        {format(currentDate, "d")}
+                      </TextAtom>
+                      {getLessonsForDay(currentDate)?.map((lesson, index) => (
+                        <div
+                          key={index}
+                          className="flex flex-col relative bg-[#BBE7B9] h-[34px] p-1 text-left rounded-[2px] gap-1 overflow-hidden border-[0.5px] border-solid border-[#bbe7b9]"
+                        >
+                          <TextAtom type={TextAtomEnum.enum_h6}>
+                            {lesson.date}
+                          </TextAtom>
+                          <TextAtom type={TextAtomEnum.enum_h6}>
+                            {lesson.title}
+                          </TextAtom>
+                          <ImageAtom
+                            type={ImageEnum.enum_defaultSvg}
+                            icon={<PaymentIcon />}
+                            className="[&>svg]:stroke-[#E12828] w-[14px] h-[14px] absolute top-[3px] right-[3px]"
+                          />
+                        </div>
+                      ))}
+                    </td>
+                  );
+                })}
               </tr>
             ))}
           </tbody>
