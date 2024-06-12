4684c56ea348ec166b0b63c17e9d4a5f51de3082
veetsok <ivan.kosteev19999@yandex.ru>
2024-06-12 06:26:31 +0300

    up

--- frontend/src/user.InterfaceLayer/Libraries/UI_KIT/Cells/ImagesTeam.Cell/index.tsx
+++ frontend/src/user.InterfaceLayer/Libraries/UI_KIT/Cells/ImagesTeam.Cell/index.tsx
@@ -15,7 +15,7 @@ const ReportBlock: React.FC<ReportBlockProps> = (props) => {
 
   return (
     <div
-      className={`${className} flex flex-col rounded-[20px] pt-4 pr-3 pb-3 pl-4 gap-[5px] max-w-[162px]`}
+      className={`${className} flex flex-col rounded-[20px] pt-4 pr-3 pb-3 pl-4 gap-[5px] w-[162px]`}
     >
       <TextAtom type={TextAtomEnum.enum_h3} className="max-w-[100px]">
         {title}
@@ -24,7 +24,7 @@ const ReportBlock: React.FC<ReportBlockProps> = (props) => {
         <ImageAtom
           type={ImageEnum.enum_defaultSvg}
           icon={icon}
-          className="py-[11px] px-[12px] rounded-[12px] w-[40px] h-[40px] bg-bg_white [&>div>svg]:w-[18px] [&>div>svg]:h-[20px] [&>div>svg]:stroke-bg_tertiary"
+          className="py-[11px] px-[12px] rounded-[12px] w-[40px] h-[40px] bg-bg_white [&>svg]:w-[18px] [&>svg]:h-[20px] [&>svg]:stroke-bg_tertiary"
         />
       </div>
     </div>
