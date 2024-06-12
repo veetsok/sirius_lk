7f638dd9b8eba4f2b019ff432d83f6a38555f4b1
veetsok <ivan.kosteev19999@yandex.ru>
2024-06-12 08:08:23 +0300

    update

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,43 +1,89 @@
-import Header from "@/user.InterfaceLayer/Components/Header";
-import MenuBlock from "@/user.InterfaceLayer/Libraries/Widgets/MenuBlock.widget";
-import { globalContainer } from "@/user.InterfaceLayer/constants/styles/CommonStyles";
-import MainLayout from "@/user.InterfaceLayer/Libraries/Layouts/Main.layout";
-import { useProducts } from "@/business.InterfaceLayer/useProducts";
-import ScheduleLayout from "@/user.InterfaceLayer/Libraries/Layouts/Schedule.layout";
-import { useCallback, useMemo, useState } from "react";
-import Spinner from "@/user.InterfaceLayer/Libraries/UI_KIT/Molecules/Spinner.Molecule";
+import ImageAtom from "@/user.InterfaceLayer/Libraries/UI_KIT/Atoms/Image.Atom";
+import ImageEnum from "@/user.InterfaceLayer/Libraries/UI_KIT/Atoms/Image.Atom/enum";
+import TextAtom from "@/user.InterfaceLayer/Libraries/UI_KIT/Atoms/Text.Atom";
+import TextAtomEnum from "@/user.InterfaceLayer/Libraries/UI_KIT/Atoms/Text.Atom/enum";
+import Link from "next/link";
+import { useState } from "react";
+import LogoIcon from "@/user.InterfaceLayer/Libraries/shared/icons/logoshort.svg?react";
 
 export default function Home() {
-  const { products, isLoading } = useProducts();
-  const [selectedSection, setSelectedSection] = useState("main");
-
-  const handleSectionChange = useCallback((section: string) => {
-    setSelectedSection(section);
-  }, []);
-
-  const renderSection = useMemo(() => {
-    switch (selectedSection) {
-      case "main":
-        return <MainLayout lessons={products} />;
-      case "schedule":
-        return <ScheduleLayout lessons={products} />;
-      default:
-        return <MainLayout lessons={products} />;
-    }
-  }, [selectedSection, products]);
-
-  if (isLoading) return <Spinner />;
+  const [translate, setTranslate] = useState(true);
 
   return (
-    <main className={`${globalContainer} text-text_primary`}>
-      <div className="flex">
-        <MenuBlock
-          activeSection={selectedSection}
-          onSectionChange={handleSectionChange}
-        />
-        <div className="flex flex-col w-full">
-          <Header />
-          <div className="ml-8 mt-5">{renderSection}</div>
+    <main
+      className={`h-[100vh] w-[100vw] flex bg-white items-center justify-center`}
+    >
+      <div className="flex flex-col pt-[50px] items-center w-[400px] h-full gap-4">
+        <ImageAtom type={ImageEnum.enum_defaultSvg} icon={<LogoIcon />} />
+        <TextAtom type={TextAtomEnum.enum_h2}>
+          {translate ? "Вход в Sirius Future" : "Login to Sirius Future"}
+        </TextAtom>
+
+        <div className="flex flex-col gap-3 w-full">
+          <input
+            type="email"
+            placeholder={translate ? "Введите E-mail" : "Enter to E-mail"}
+            className="w-full border-[1px] border-solid border-[#ececec] rounded-[8px] py-[10px] px-3 bg-white"
+          />
+          <input
+            type="password"
+            placeholder={translate ? "Введите пароль" : "Enter to password"}
+            className="w-full border-[1px] border-solid border-[#ececec] rounded-[8px] py-[10px] px-3 bg-white"
+          />
+          <div className="flex gap-1 items-center">
+            <input
+              type="checkbox"
+              name=""
+              id=""
+              className="w-[22px] h-[22px] rounded-[5px] border-[1px] border-solid border-[#ececec]"
+            />
+            <TextAtom type={TextAtomEnum.enum_h4} className="text-[#79747f]">
+              {translate ? "Запомнить меня" : "Remember me"}
+            </TextAtom>
+          </div>
+        </div>
+
+        <div className="flex w-full justify-between">
+          <Link href="/profile">
+            <TextAtom type={TextAtomEnum.enum_h4} className="text-text_success">
+              {translate ? "Я забыл пароль" : "I forgot password"}
+            </TextAtom>
+          </Link>
+          <Link href="/profile">
+            <TextAtom type={TextAtomEnum.enum_h4} className="text-text_success">
+              {translate ? "Войти как тренер" : "Login as a coach"}
+            </TextAtom>
+          </Link>
+        </div>
+        <div className="flex flex-col text-center">
+          <TextAtom type={TextAtomEnum.enum_h4}>
+            {translate ? "Нет аккаунта?" : "Don't have an account?"}
+          </TextAtom>
+          <TextAtom type={TextAtomEnum.enum_h4} className="text-text_success">
+            {translate ? "Зарегистрироваться" : "Register"}
+          </TextAtom>
+        </div>
+        <div className="flex gap-2 w-full justify-center items-center">
+          <button
+            className={`${
+              translate
+                ? "[&>h4]:!text-[24px] text-[#7362bc]"
+                : "text-[#79747f]"
+            }`}
+            onClick={() => setTranslate(true)}
+          >
+            <TextAtom type={TextAtomEnum.enum_h4}>RU</TextAtom>
+          </button>
+          <button
+            className={`${
+              !translate
+                ? "[&>h4]:!text-[24px] text-[#7362bc]"
+                : "text-[#79747f]"
+            }`}
+            onClick={() => setTranslate(false)}
+          >
+            <TextAtom type={TextAtomEnum.enum_h4}>EN</TextAtom>
+          </button>
         </div>
       </div>
     </main>
---
+++ frontend/src/pages/profile/index.tsx
@@ -0,0 +1,45 @@
+import Header from "@/user.InterfaceLayer/Components/Header";
+import MenuBlock from "@/user.InterfaceLayer/Libraries/Widgets/MenuBlock.widget";
+import { globalContainer } from "@/user.InterfaceLayer/constants/styles/CommonStyles";
+import MainLayout from "@/user.InterfaceLayer/Libraries/Layouts/Main.layout";
+import { useProducts } from "@/business.InterfaceLayer/useProducts";
+import ScheduleLayout from "@/user.InterfaceLayer/Libraries/Layouts/Schedule.layout";
+import { useCallback, useMemo, useState } from "react";
+import Spinner from "@/user.InterfaceLayer/Libraries/UI_KIT/Molecules/Spinner.Molecule";
+
+export default function Profile() {
+  const { products, isLoading } = useProducts();
+  const [selectedSection, setSelectedSection] = useState("main");
+
+  const handleSectionChange = useCallback((section: string) => {
+    setSelectedSection(section);
+  }, []);
+
+  const renderSection = useMemo(() => {
+    switch (selectedSection) {
+      case "main":
+        return <MainLayout lessons={products} />;
+      case "schedule":
+        return <ScheduleLayout lessons={products} />;
+      default:
+        return <MainLayout lessons={products} />;
+    }
+  }, [selectedSection, products]);
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <main className={`${globalContainer} text-text_primary`}>
+      <div className="flex">
+        <MenuBlock
+          activeSection={selectedSection}
+          onSectionChange={handleSectionChange}
+        />
+        <div className="flex flex-col w-full">
+          <Header />
+          <div className="ml-8 mt-5">{renderSection}</div>
+        </div>
+      </div>
+    </main>
+  );
+}
